25f17bebd0c5d4a24ad5cc2a576eb621524fe3d9
Roger Zhuang <zhuangziyin@gmail.com>
2022-11-23 12:42:21 -0800

    run only once

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,8 @@ const clickMeButton = document.querySelector(".click-me");
 function clickHandler ()
 {
     alert("You clicked me!");
+    // clickMeButton.removeEventListener("click", clickHandler);
 }
 
-clickMeButton.addEventListener("click", clickHandler);
+clickMeButton.addEventListener("click", clickHandler, {once:true});
+
